63ea1edad4abf1a33462eec05b96ca6207ba0428
Andrea Delgado Herrera <delgadoherreraandrea@Andrea-Andrea-Viviana-Delgado-C02FV3SPML7H.local>
2023-03-18 14:49:30 -0300

    my first api

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const app = express();
+const userRoutes = require('./routes/user.js');
+
+// la variable de ambiente PORT es propia de nodejs. Si no usa el puerto 3000 usar el 3001
+const port = process.env.PORT || 3001; 
+
+//middleware (puente entre la aplicación del servidor y los clientes que solicitan recursos a través de la API)
+// en el middleware se hacen validaciones y transformaciones de datos, autenticacion y autorizacion de usuarios, cache de respuestas (para que sea mas rapido)
+app.use(express.json());
+app.use('/api',userRoutes) ;
+
+
+//definis la ruta principal de la api
+app.get('/', (req,res) => {res.send("Hello world")});
+
+//te conectas a la base de datos Mongo
+mongoose
+.connect(process.env.DBMONGO_URI)
+.then( () => console.log("Conexion Mongo DB Atlas OK"))
+.catch( (error) => console.log(error));
+
+//pones a escuchar en un determinado puerto
+app.listen(port, ()=> console.log('Servidor web funcionando',port));
---
+++ src/routes/user.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const userSchema = require("../models/user");
+
+const router = express.Router();
+
+// endpoint crear usuario
+router.post('/users', (req,res)=> {
+    
+   const user = userSchema(req.body);
+   user
+   .save()
+   .then((data) => res.json(data))
+   .catch((error) => res.json({ message: error }));
+
+});
+
+module.exports = router;
